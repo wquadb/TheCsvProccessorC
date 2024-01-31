c62dca09dcaf3fd9a5a91f1d7e02ff758b02922a
Evgeny <wwwquadb@gmail.com>
2024-01-31 17:34:22 +0300

    Initialised the project

---
+++ main.c
@@ -0,0 +1,17 @@
+// Инструменты ввода-вывода
+#include <stdio.h>
+// Стандартная библиотека
+#include <stdlib.h>
+// Инструменты для работы с строками
+#include <string.h>
+// Структура данных "очередь" от BSD
+#include <sys/queue.h>
+// Научные инструменты GSL
+#include <gsl/gsl_fit.h>
+#include <gsl/gsl_statistics_double.h>
+
+
+int main(){
+    printf("Hello world!");
+    return 0;
+}
