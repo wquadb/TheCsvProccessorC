a8a2eae1903cdad0e3e4d75dd35f726bbd91a4f9
wquadb <esavdeev@edu.hse.ru>
2024-02-02 21:21:47 +0300

    start

--- main.c
+++ main.c
@@ -6,12 +6,20 @@
 #include <string.h>
 // Структура данных "очередь" от BSD
 #include <sys/queue.h>
+// Тип данных bool
+#include <stdbool.h>
 // Научные инструменты GSL
 #include <gsl/gsl_fit.h>
 #include <gsl/gsl_statistics_double.h>
 
+// before initialization new big project, solve some common problems on the internet
 
-int main(){
+int main()
+{
+    const char *input_file_name = "data.csv";
+    const char *delimiter = "\t";
+    const char *output_file_name = "fit_C99.csv";
+    const unsigned int N = 100;
     printf("Hello world!");
     return 0;
 }
